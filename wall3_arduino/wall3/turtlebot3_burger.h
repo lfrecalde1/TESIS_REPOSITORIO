fc8557949e2e412d87b6a35ab8efe9b30c825be4
lfrecalde1 <lfrecalde1@espe.edu.ec>
2021-03-02 16:12:14 -0500

    Simulador Implementado en webots

---
+++ wall3_arduino/wall3/turtlebot3_burger.h
@@ -0,0 +1,19 @@
+#ifndef TURTLEBOT3_BURGER_H_
+#define TURTLEBOT3_BURGER_H_
+
+#define NAME                             "Burger"
+
+#define WHEEL_RADIUS                     0.033           // meter
+#define WHEEL_SEPARATION                 0.216           // meter (BURGER : 0.160, WAFFLE : 0.287)
+#define TURNING_RADIUS                   0.108           // meter (BURGER : 0.080, WAFFLE : 0.1435)
+#define ROBOT_RADIUS                     0.155           // meter (BURGER : 0.105, WAFFLE : 0.220)
+#define ENCODER_MIN                      -2147483648     // raw
+#define ENCODER_MAX                      2147483648      // raw
+
+#define MAX_LINEAR_VELOCITY              (WHEEL_RADIUS * 2 * 3.14159265359 * 61 / 60) // m/s  (BURGER : 61[rpm], WAFFLE : 77[rpm])
+#define MAX_ANGULAR_VELOCITY             (MAX_LINEAR_VELOCITY / TURNING_RADIUS)       // rad/s
+
+#define MIN_LINEAR_VELOCITY              -MAX_LINEAR_VELOCITY  
+#define MIN_ANGULAR_VELOCITY             -MAX_ANGULAR_VELOCITY 
+
+#endif  //TURTLEBOT3_BURGER_H_
